9629ded3cf73e5b8b0e6b9eae9bd776c8d4757ce
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2021-12-24 23:56:21 +0200

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,33 @@
+const User = require('../models/User')
+
+exports.register = async (req, res, next) => {
+    const {username, email, password} = req.body;
+
+    try {
+        const user = await User.create({
+            username, email, password
+        })
+
+        res.status(201).json({
+            success : true,
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
+}
+
+exports.login = (req, res, next) => {
+    res.send('Login route')
+}
+
+exports.forgotpassword = (req, res, next) => {
+    res.send('forgot password route')
+}
+
+exports.resetpassword = (req, res, next) => {
+    res.send('reset password route')
+}
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+        match: [
+            /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+            "Please provide a valid email"
+        ]
+    },
+    password : {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken : String,
+    resetPasswordExpire : Date
+});
+
+UserSchema.pre("save", async function(next) {
+    if(!this.isModified("password")){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
